969137a0c71aa9859712febc1e48e659be9659bb
chrisanthropic <ctarwater@gmail.com>
2017-01-27 18:23:39 -0800

    cleanup plexrequests install script

--- 02b-plexrequests.sh
+++ 02b-plexrequests.sh
@@ -16,16 +16,9 @@ fi
 #######################
 # Install
 #######################
-exec sudo -i -u $username /bin/bash - << eof
 curl "https://install.meteor.com/?release=1.2.1" | sh
 sudo git clone https://github.com/lokenx/plexrequests-meteor.git /opt/plexrequests/
 sudo chown -R $username:$username /opt/plexrequests
-cd /opt/plexrequests
-meteor &
-PID=$!
-sleep 10m
-kill $PID
-eof
 
 #######################
 # Systemd Service File
@@ -65,7 +58,7 @@ echo ''
 echo "Would you like us to open the port in UFW?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) ufw allow 3000; echo ''; echo "Port 3000 open, PlexRequests is now available over the internet."; echo ''; break;;
+        Yes ) ufw allow 3000; echo ''; echo "Port 3000 open, PlexRequests is now available over the internet at $ipaddr:3000."; echo ''; break;;
         No ) echo "Port 3000 left closed. You can still access it on your local machine by issuing the following command: ssh $username@$ipaddr -L 3000:localhost:3000"; echo "and then open localhost:3000 on your browser."; exit;;
     esac
 done
